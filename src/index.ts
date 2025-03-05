4c4f63b10a06dc6c5c02d553492d9ceb1585fdbe
mahatsumit5 <mahatsumit5@gmail.com>
2025-03-05 14:39:29 +1100

    apollo server ready

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-// TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
+import { gql } from "graphql-tag";
+import { resolve } from "path";
+
+const typeDefs = gql(
+  readFileSync(resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
+
+async function startApolloServer() {
+  const { url } = await startStandaloneServer(new ApolloServer({ typeDefs }));
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+startApolloServer();
